3221019ea75f2b18fb67f37513ba4a688e0afccc
Alexander Yushkov <alexanderyushkov@Alexanders-MacBook-Pro.local>
2021-04-19 19:34:23 -0500

    HM done

---
+++ functions.py
@@ -0,0 +1,28 @@
+# Modify this function to return a list of strings as defined above
+
+def list_benefits():
+    list_benefits = ['More organized code',
+                     'More readable code',
+                     'Easier code reuse',
+                     'Allowing programmers to share and connect code together']
+    return(list_benefits)
+
+# Modify this function to concatenate to each benefit - " is a benefit of functions!"
+
+def build_sentence(benefit):
+    return(benefit + " is a benefit of functions!")
+
+
+def name_the_benefits_of_functions():
+
+    list_of_benefits = list_benefits()
+
+    print(build_sentence(list_of_benefits[0]))
+
+    print(build_sentence(list_of_benefits[1]))
+
+    print(build_sentence(list_of_benefits[2]))
+
+    print(build_sentence(list_of_benefits[3]))
+
+name_the_benefits_of_functions()
